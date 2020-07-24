7e16e521bb3ad03b65b4e441f7299828a1e26359
Steven <stevenrockett@gmail.com>
2020-07-24 14:14:21 +0100

    another footer fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return (
-    <div className="row justify-content-center py-3">
-      Developed by Steven Rockett
-    </div>
-  );
+  return <div className="text-center py-3">Developed by Steven Rockett</div>;
 };
 
 export default Footer;
